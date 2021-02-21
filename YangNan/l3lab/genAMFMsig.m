4328ab131f9cfc4cdba837c4c461b6c77f874c5a
RamboYoung007700 <cqunanyang@hotmail.com>
2021-02-22 02:41:15 +0800

    add the last 3 signals.

--- YangNan/l3lab/genAMFMsig.m
+++ YangNan/l3lab/genAMFMsig.m
@@ -1,7 +1,7 @@
 function sigVec = genAMFMsig(dataX,snr,b,f0,f1)
 % Generate a  AM-FM signal
 % S = GENAMSIG(X,SNR,B,F0,F1)
-% Generates a AM signal S. X is the vector of
+% Generates a AM-FM signal S. X is the vector of
 % time stamps at which the samples of the signal are to be computed. SNR is
 % the matched filtering signal-to-noise ratio of S. B is the coefficient of
 % modulated partf. F0 and F1 are frequencies. 
