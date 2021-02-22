1e6e910c87955bc249066dbe895618d6e3c057c5
soupbridge <452644468@qq.com>
2021-02-22 15:58:27 +0800

    debug

--- Tangqiao/SIGNALS/testcbgensinsig.m
+++ Tangqiao/SIGNALS/testcbgensinsig.m
@@ -1,13 +1,19 @@
-%% Plot the sin  signal
+%% Plot the quadratic chirp signal
 % Signal parameters
+snr =10
+f0 =10
+pha =1
+% Instantaneous frequency after 1 sec is 
+maxfreq = f0
+samplFreq = 10 *maxfreq;
+samplIntrvl = 1/samplFreq;
 
-fre = 1
-pha = 0.4
-snr = 1
 % Time samples
-timeVec = 0:0.05:2;
+timeVec = 0:samplIntrvl:1.0;
+
+% Generate the signal
+ sigVect = crcbgensinsig(timeVec,snr,pha,f0)
 
 %Plot the signal 
-sigVect = crcbgensinsig(timeVec,snr,pha,fre)
-figure
+figure;
 plot(timeVec,sigVect,'Marker','.','MarkerSize',24);
--- Tangqiao/SiGNALS/testgenlcsig.m
+++ Tangqiao/SiGNALS/testgenlcsig.m
@@ -5,9 +5,10 @@ f0 =10
 f1 =3
 pha =1
 % Instantaneous frequency after 1 sec is 
-maxfreq = f0
-samplFreq = 10*maxfreq;
-samplIntrvl = 1/samplFreq;
+maxFreq = f0 + 2 * f1 * 1
+samplFreq = 10 * maxFreq;
+samplIntrvl = 1  /samplFreq;
+
 
 % Time samples
 timeVec = 0:samplIntrvl:1.0;
