c55309c6ae901563d98615ed67a576d234ceb4f2
soupbridge <452644468@qq.com>
2021-02-22 15:39:51 +0800

    add some new content

--- Tangqiao/SiGNALS/testgenlcsig.m
+++ Tangqiao/SiGNALS/testgenlcsig.m
@@ -1,11 +1,16 @@
 %% Plot the quadratic chirp signal
 % Signal parameters
-snr =1
-f0 =3
-f1 =2
+snr =10
+f0 =10
+f1 =3
 pha =1
+% Instantaneous frequency after 1 sec is 
+maxfreq = f0
+samplFreq = 10*maxfreq;
+samplIntrvl = 1/samplFreq;
+
 % Time samples
-timeVec = 0:0.01:1.5;
+timeVec = 0:samplIntrvl:1.0;
 
 % Generate the signal
 sigVec = genlcsig(timeVec,snr,[f0,f1],pha)
